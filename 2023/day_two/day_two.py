dbe1f35b0d8a6e755bd3feafefa6f1fac9aaa5a5
Nikk Jackson <NikkolasJ@Outlook.com>
2024-01-19 20:08:38 -0500

    feat: completed day two part two

--- 2023/day_two/day_two.py
+++ 2023/day_two/day_two.py
@@ -1,3 +1,4 @@
+from os import walk
 import re
 
 def get_input() -> list[str]:
@@ -49,11 +50,63 @@ def is_game_legal(game: str, id: int) -> bool:
             continue
     return True
 
+def get_largest_cube_amount_from_game(game: str, id: int) -> list[int]:
+    largest_red = None
+    largest_green = None
+    largest_blue = None
+    hands = seperate_hands(game, id)
+    for hand in hands:
+        hand_info = seperaate_color_and_number(hand)
+        number = int(hand_info[0])
+        color = hand_info[1]
+        if color == "red":
+            if largest_red is None:
+                largest_red = number
+            elif largest_red < number:
+                largest_red = number
+        elif color == "green":
+            if largest_green is None:
+                largest_green = number
+            elif largest_green < number:
+                largest_green = number
+        else:
+            if largest_blue is None:
+                largest_blue = number
+            elif largest_blue < number:
+                largest_blue = number
+    if largest_red is not None and largest_green is not None and largest_blue is not None:
+        return [largest_red, largest_green, largest_blue]
+    else:
+        return [0, 0, 0]
+
+def get_smallest_cube_amounts(games: list[str]) -> list[list[int]]:
+    largest_games = []
+    for game in games:
+        id = get_game_id(game)
+        largest_values = get_largest_cube_amount_from_game(game, id)
+        largest_games.append(largest_values)
+    return largest_games
+
+def get_power_of_list(values: list[int]) -> int:
+    return values[0] * values[1] * values[2]
+
+def get_all_powers_of_games(numbers: list[list[int]]) -> list[int]:
+    result = []
+    for number in numbers:
+        values = get_power_of_list(number)
+        result.append(values)
+    return result
+
+
 def main() -> int:
     games = get_input()
     game_ids = get_valid_games(games)
     sum_of_ids = sum_list(game_ids)
     print(sum_of_ids)
+    largest_amounts = get_smallest_cube_amounts(games)
+    powers = get_all_powers_of_games(largest_amounts)
+    sum_of_powers = sum_list(powers)
+    print(sum_of_powers)
     return 0
 
 main()
